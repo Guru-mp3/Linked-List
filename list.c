75cd1e37fd9a9ceb7bc42612d34059a2777d4cd5
Gurumoorty Suresh <54660832+Guru-mp3@users.noreply.github.com>
2019-10-18 00:13:00 +0530

    Add files via upload

---
+++ list.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+struct node
+{
+	int info;
+	struct node *next;
+};
+int main()
+{
+	struct node *p1,*p2,*p3,*temp;
+	p1=(struct node*)malloc(sizeof(struct node));
+	p2=(struct node*)malloc(sizeof(struct node));
+	p3=(struct node*)malloc(sizeof (struct node));
+	p1->info=20;
+	p2->info=30;
+	p3->info=45;
+	p1->next=p2;
+	p2->next=p3;
+	p3->next=NULL;
+	temp=p1;
+	while(temp!=NULL)
+	{
+	printf("data=%d\npresent address=%u\nnext address=%u\n\n",temp->info,temp,temp->next);
+	temp=temp->next;
+}
+
+
+	return 0;
+
+}
